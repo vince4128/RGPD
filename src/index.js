dce5f15aeeeee1bc4367e4fd773c04f3c34e9641
vince4128 <vrlt.vincent@gmail.com>
2017-10-03 16:42:29 +0200

    core.module added

--- src/index.js
+++ src/index.js
@@ -9,4 +9,5 @@ require("./app/view/layout/layout.html");
 
 //modules
 require("./app/app.module.js");
+require("./app/core/core.module.js");
 require("../lib/angularjs-scorm-wrapper/angularjs-scorm-wrapper.min.js");
