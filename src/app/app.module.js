2d12ad60ac74e73926df9ef074481c13130b9c5e
vince4128 <vrlt.vincent@gmail.com>
2017-10-03 16:13:13 +0200

    app.module added

---
+++ src/app/app.module.js
@@ -0,0 +1,8 @@
+(function(){ //Fonction immédiatemment invoquée (pour éviter d'avoir des variables globales)
+
+    'use strict';//Pour éviter des variables non déclarées ou d'autres dégueulasserie
+
+    angular
+        .module('app',[])//Déclaration du module principal
+
+}());
