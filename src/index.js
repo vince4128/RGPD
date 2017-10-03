3a54ed6a347b62630c95c608104f57a64757ebdd
vince4128 <vrlt.vincent@gmail.com>
2017-10-03 16:12:54 +0200

    index.js added

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*  styles  */
+require("./scss/style.scss");
+
+/*  html    */
+require("./app/view/layout/layout.html");
+
+/*  js  */
+//Attention à l'ordre d'importation des fichiers
+
+//modules
+require("./app/app.module.js");
+require("../lib/angularjs-scorm-wrapper/angularjs-scorm-wrapper.min.js");
