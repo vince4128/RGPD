cc472dfc625766f7b0cd2819682de4555297853d
vince4128 <vrlt.vincent@gmail.com>
2017-10-04 11:50:20 +0200

    newmodule exemple added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ require("./scss/style.scss");
 
 /*  html    */
 require("./app/view/layout/layout.html");
+require("./app/view/newmodule/newmodule.html");
 
 /*  js  */
 //Attention à l'ordre d'importation des fichiers
 
-//modules
+/* Angular */
+
+/* Angular - module */
 require("./app/app.module.js");
 require("./app/core/core.module.js");
 require("../lib/angularjs-scorm-wrapper/angularjs-scorm-wrapper.min.js");
+//
+require("./app/newmodule/newmodule.module.js");
+
+/* Angular - directives */
+
+/* Angular - services */
+
+/* Angular - routes */
+require('./app/newmodule/newmodule.route.js');
+
+/* Angular - controllers */
+require('./app/newmodule/NewmoduleController.js');
+
+
+/* Data */
