cfd926c5ad5e214bd3307100c28ab1a8017a1a07
vince4128 <vrlt.vincent@gmail.com>
2017-10-05 12:02:09 +0200

    scorm get version init session and send time onbeforeunload tested on ganesha

--- src/app/service/scorm.service.js
+++ src/app/service/scorm.service.js
@@ -8,35 +8,132 @@
     scormService.$inject = ['scormWrapper'];
 
     function scormService(scormWrapper) {
+        //retourne un objet dans lequel on expose les méthodes et variables "publics"
         var service = {
+            //initialisation et clôture
             initScorm: initScorm,
-            scormApiVersion:scormApiVersion
+            endScorm: endScorm,
+            //
+            sendSessionTime: sendSessionTime,
+            //
+            scormApiVersion:scormApiVersion,
+            //getter et setter
+            getScormVersion:getScormVersion
         };
 
-        //variable ici (avant le return)
+        //variable ici (avant le return sinon elles sont undefined)
         var startTime;
         var scormApiVersion = "";
         var objSuspend = {};
 
+        //appel de la méthode privée d'initialisation du service
+        activate();
+
+        //on retourne l'objet service
         return service;
         ///////////////
 
+        /* Méthodes public (exposées) */
+
+        //méthode d'initialisation de scorm
         function initScorm() {
             //Quelle version de scorm ?//
-            //modif a faire apparaitre//
             console.log('# scormService lance setAPIVersion #');
             scormWrapper.setAPIVersion("Auto");
             //Initialiser le scorm
-            scormWrapper.doLMSInitialize("cmi.core.lesson_status","incomplete");
-            var success = scormWrapper.isAvailable;
+            scormWrapper.doLMSInitialize();
+            var success = scormWrapper.isAvailable();
             console.log("# scormService Scorm init status : " + success);
             if(success){
               console.log("# scormService : connexion scorm initialisé");
-              var apiVersion = scormWrapper.getAPIVersion();
-              console.log("# scormService : version de scorm : " + apiVersion);  
+              scormApiVersion = scormWrapper.getAPIVersion();
+              console.log("# scormService : version de scorm : " + scormApiVersion);  
             } else {
               console.log("# scormService : connexion scorm pas initialisé");
             }
          }
+
+        //méthode de clôture de la sessison scorm
+        function endScorm(){
+
+            //a supp une fois ok
+            console.log("# scormService : ça va couper !");
+
+            //on envoie le temps
+            sendSessionTime();
+            scormWrapper.doLMSCommit();
+            scormWrapper.doLMSFinish();
+
+        }
+
+        //méthode d'envoi du temps
+        function sendSessionTime(){
+
+            console.log("# scormService Session Time à envoyer");
+
+            var dtm = new Date();
+            var n = dtm.getTime() - startTime;
+
+            if(scormApiVersion === "1.2"){
+
+                //a supp une fois OK
+                console.log("# scormService : envoi du temps du temps pour du Scorm " + scormApiVersion);
+
+                scormWrapper.doLMSSetValue('cmi.core.session_time',convertTime(n));
+
+            }else{
+
+                //a supp une fois OK
+                console.log("# scormService Le code pour scorm 2004 n'a pas encore été écrit");
+
+            }
+            
+        }
+
+        //getter & setter
+
+        //méthode de récupération de la version de scorm
+        function getScormVersion(){
+            if(scormApiVersion){
+                return scormApiVersion;
+            }
+        }
+
+        /* Méthodes privée (non exposées) */
+
+        //méthode privée d'initialisation du service
+        function activate(){
+            //on récupère la date pour le calcul du temps de la session
+            startTime = new Date().getTime();
+        }
+
+        //Méthode de conversion du temps
+        function convertTime(n){
+
+            //a supp une fois OK
+            console.log("# scormService : conversion du temps");
+
+            if(scormApiVersion === "1.2"){
+
+                //a supp une fois OK
+                console.log("# scormService : conversion du temps pour du Scorm " + scormApiVersion);
+
+                var hms = "";
+                var dtm = new Date();
+                dtm.setTime(n);
+                var h = "0" + Math.floor(n/3600000);
+                var m = "0" + dtm.getMinutes();
+                var s = "0" + dtm.getSeconds();
+                hms = h.substr(h.length-2)+":"+m.substr(m.length-2)+":";
+                hms += s.substr(s.length-2);
+
+                //a supp une fois OK
+                console.log("# scormService : temps à envoyer " + hms);
+
+                return hms;
+
+            }
+        }
+
     }
 })();
