887c476f96c6433dfb29c850829eec2e8a18f032
vince4128 <vrlt.vincent@gmail.com>
2017-10-05 12:01:14 +0200

    closing scorm session  when onbeforeunload is called

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -26,5 +26,11 @@ function runApp($window, scormService){
     var vm = this;
 
     scormService.initScorm();
+
+    //on attache un ecouteur d'évènement sur la fermeture de la fenêtre
+    $window.onbeforeunload = function(event) {
+        event.preventDefault(); //on empêche la fermeture de la fenêtre
+        scormService.endScorm(); //LMSFinish va fermer la fenêtre tout seul
+    }
     
 }
