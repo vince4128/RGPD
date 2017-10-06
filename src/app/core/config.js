0c2d546736e6cc884012371e92a4efa30382bcae
vince4128 <vrlt.vincent@gmail.com>
2017-10-06 15:31:44 +0200

    translate provider added, obj trad added

--- src/app/core/config.js
+++ src/app/core/config.js
@@ -10,18 +10,31 @@
     configure.$inject = ['$logProvider','$exceptionHandlerProvider','$translateProvider']
 
     function configure($logProvider,$exceptionHandlerProvider,$translateProvider) {
-        
+
         var french = {
-            "Title":"Internationalisation",
-            "Language":"Langues",
-            "test":"test français"
+            "tradObj":{
+                "TITLE_ID":"Internationalisation",
+                "LANGUAGE_ID":"Langues",
+                "TEST_ID":"test français",
+                "TEST_HTML":"<h4>Démo</h4> avec des balises html à interpréter",
+                "TEST_PLURAL_ID":"{testCount, plural, =0{Résultat nul}, one{Résultat 1}, other{# Messages}}",//messageformat.js
+                "TEST_GREETING_ID":"Salut {{name}} !",
+                "TEST_KEY_IN_HTML":"Clé directement placé dans le .html",
+                "TEST_WITH_FILTER_ID":"Traduis en utilisant un filtre"
+            }
         }
 
         var english = {
-            "Title":"Internationalization",
-            "Language":"Language",
-            "test":"test anglais"
+            "tradObj":{
+                "TITLE_ID":"Internationalization",
+                "LANGUAGE_ID":"Language",
+                "TEST_ID":"test anglais"
+            }
         }
+
+        $translateProvider.translations('fr', french);
+        $translateProvider.preferredLanguage('fr');
+        $translateProvider.fallbackLanguage('fr');
 
     }
 
