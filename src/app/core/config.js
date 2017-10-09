3c5ad098dedffc621ba966b42bd778c5ccf805b0
vince4128 <vrlt.vincent@gmail.com>
2017-10-09 10:04:10 +0200

    error detection, available languages handmade fallback language added

--- src/app/core/config.js
+++ src/app/core/config.js
@@ -4,20 +4,41 @@
     angular
         .module('app.core')
         .config(configure)
+        .run(runApp)
 
     configure.$inject = ['$logProvider', '$exceptionHandlerProvider', '$translateProvider']
 
     function configure($logProvider, $exceptionHandlerProvider, $translateProvider) {
 
         $translateProvider
-            .preferredLanguage('fr')
+            .registerAvailableLanguageKeys(
+            ['fr', 'en'],
+            {
+                'en*': 'en',
+                'fr*': 'fr',
+                '*': 'en' // doit être en dernier (fallback "bricolé").
+            }
+            )
+            .preferredLanguage('fr')//le langage que nous voulons utiliser
+            .fallbackLanguage('en')//si un langage par ex cz est choisi alors qu'il n'existe pas alors l'anglais sera présenté (fonctionne pas pour l'instant)
             .useStaticFilesLoader({//les fichiers doivent être nommés comme les clés utilisés (fr,en,etc...)
                 prefix: '/app/lang/',
                 suffix: '.json'
             })
-            .useSanitizeValueStrategy('sanitize')
-            .fallbackLanguage('fr');
+            .useSanitizeValueStrategy('sanitize');
 
     }
+
+    runApp.$inject = ['$rootScope'];
+
+    function runApp($rootScope) {
+        $rootScope.$on('$translateChangeSuccess', function () {
+            console.log('Translation change success!');
+        });
+        $rootScope.$on('$translateChangeError', function () {
+            console.log('Translation change Error!');
+        });
+    }
+
 
 }());
