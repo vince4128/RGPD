8672dd58105bfc72bd2fe28c5975313211707424
vince4128 <vrlt.vincent@gmail.com>
2017-10-09 17:02:48 +0200

    changeLanguage method deleted see Internationalization example to use it

--- src/app/newmodule/NewmoduleController.js
+++ src/app/newmodule/NewmoduleController.js
@@ -1,4 +1,4 @@
-(function () {
+(function(){
     'use strict';
 
     angular
@@ -17,18 +17,5 @@
         activate();
 
         function activate() { }
-
-        function changeLanguage(langKey) {
-            $translate.use(langKey);
-        }
-
-        //faire une traduction dans le code
-        //ne fonctionne pas, à vérifier
-        //alert(angular.toJson($translate('tradObj.TEST_HTML')));
-
-        $rootScope.$on('$translateChangeSuccess', function(event,data){//au changement de langage
-            vm.langue = data.language;
-        });
-
     }
 })();
