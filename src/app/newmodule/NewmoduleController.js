236e6b73254b9273fa9eac3b7f065b450271420c
vince4128 <vrlt.vincent@gmail.com>
2017-10-09 15:13:15 +0200

    current language accessible from controller

--- src/app/newmodule/NewmoduleController.js
+++ src/app/newmodule/NewmoduleController.js
@@ -5,11 +5,14 @@
         .module('app.newmodule')
         .controller('NewmoduleCtrl', NewmoduleCtrl)
 
-    NewmoduleCtrl.$inject = ['$translate'];
+    NewmoduleCtrl.$inject = ['$translate','$locale','$rootScope'];
 
-    function NewmoduleCtrl($translate) {
+    function NewmoduleCtrl($translate,$locale,$rootScope) {
         /* jshint validthis:true */
         var vm = this;
+
+        vm.langue = $translate.use();//utile pour les images
+        // + voir l'écouteur sur le $rootscope plus bas pour la maj de cette valeur
 
         vm.atraduire = ".TEST_HTML";
         vm.langChoice = "";
@@ -26,6 +29,10 @@
         //faire une traduction dans le code
         //ne fonctionne pas, à vérifier
         //alert(angular.toJson($translate('tradObj.TEST_HTML')));
-        
+
+        $rootScope.$on('$translateChangeSuccess', function(event,data){//au changement de langage
+            vm.langue = data.language;
+        });
+
     }
 })();
