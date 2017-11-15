bb489bff0e38bbb3241505d7264e91e6f7a5a9b7
sberthe <sberthe@cylande.com>
2017-11-15 16:50:03 +0100

    bugfix

--- src/app/core/core.module.js
+++ src/app/core/core.module.js
@@ -5,7 +5,7 @@
             /*
             *   Angular module
             */
-            'ui.router','ngTouch','ngAnimate','ngSanitize','ui-notification','ngDialog','pascalprecht.translate', 'tmh.dynamicLocale',
+            'ui.router','ngTouch','ngAnimate','ngSanitize','ui-notification','ngDialog','pascalprecht.translate',
             /*
             *   Scorm Wrapper
             */
