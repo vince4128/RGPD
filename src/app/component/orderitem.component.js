89c73ac7f20e5d6c0653c9c6fe0c5f20e4da55f5
sberthe <sberthe@cylande.com>
2017-11-22 14:46:18 +0100

    orderlist activity with animations

--- src/app/component/orderitem.component.js
+++ src/app/component/orderitem.component.js
@@ -7,25 +7,34 @@
             bindings: {
                 item: '<',
                 max: '<',
+                index: '<',
                 updateList: '&'
             },
             templateUrl: './app/template/orderlist/orderitem.html',
             controller: orderitemController
         });
 
-    orderitemController.$inject = [];
-    function orderitemController() {
+    orderitemController.$inject = ['$element', '$scope'];
+    function orderitemController($element, $scope) {
         var ctrl = this;
 
         ctrl.$onInit = function () {
             //
+            $element.css({
+                'top': ctrl.item.index * 130 + 'px'
+            });
         };
 
+        ctrl.$onChanges = function(changesObj){
+            //console.log("changesObj", changesObj);
+            $element.css({
+                'top': ctrl.item.index * 130 + 'px'
+            });
+        }
+        
         ctrl.move = function (direction) {
             var oldIndex = ctrl.item.index;
             var newIndex = ctrl.item.index + direction;
-
-            //ctrl.item.index += direction;
 
             ctrl.updateList({ item: ctrl.item, oldIndex: oldIndex, newIndex: newIndex });
         };
--- src/app/component/orderlist.component.js
+++ src/app/component/orderlist.component.js
@@ -11,24 +11,24 @@
             controller: orderlistController
         });
 
-    orderlistController.$inject = ['orderByFilter', '$scope'];
-    function orderlistController(orderBy, $scope) {
+    orderlistController.$inject = ['orderByFilter', '$scope', '$rootScope'];
+    function orderlistController(orderBy, $scope, $rootScope) {
         var ctrl = this;
         ctrl.shuffledItems = [];
         ctrl.correctionBtnVisible = false;
 
-        ctrl.$onInit = function () {
-            //console.log("orderitems", ctrl.item.content.orderitems);
+        $rootScope.currItemIndex = 0;
 
+        ctrl.$onInit = function () {
             //make a copy of items so the shuffling may never return the original order
             angular.copy(ctrl.item.content.orderitems, ctrl.shuffledItems);
 
             //create an array of indices as many of items, ex: [0,1,2,3]
             var shuffledIndices = Array.apply(null, { length: ctrl.shuffledItems.length }).map(Function.call, Number);
             //and shuffle it, ex: [3,1,0,2]
-            shuffle(shuffledIndices);
-
-            //console.log("shuffledIndices", shuffledIndices);
+            if (!ctrl.item.read) {
+                shuffle(shuffledIndices);
+            }
 
             //create index property on list items, and set it shuffledIndices
             for (var i = 0; i < ctrl.item.content.orderitems.length; i++) {
@@ -38,17 +38,34 @@
             }
 
             //sort by shuffled indices
-            ctrl.shuffledItems = orderBy(ctrl.shuffledItems, 'index', false);
-
-            //console.log("shuffledItems", ctrl.shuffledItems);
+            if (!ctrl.item.read) {
+                ctrl.shuffledItems = orderBy(ctrl.shuffledItems, 'index', false);
+            }
         };
 
         ctrl.update = function (item, oldIndex, newIndex) {
+            
+            var oldItem = getItemByIndex(oldIndex);
+            var newItem = getItemByIndex(newIndex);
+
+            oldItem.index = newIndex;
+            newItem.index = oldIndex;
+
+            /*
+            // --- AVEC TRI / SANS ANIMATIONS
             ctrl.shuffledItems[oldIndex].index = newIndex;
             ctrl.shuffledItems[newIndex].index = oldIndex;
 
             //tri sur index property
             ctrl.shuffledItems = orderBy(ctrl.shuffledItems, 'index', false);
+            */
+        };
+
+        function getItemByIndex(index) {
+            function findIndex(item) {
+                return item.index === index;
+            }
+            return ctrl.shuffledItems.find(findIndex);
         };
 
         ctrl.validate = function () {
@@ -56,14 +73,13 @@
             var result = 0;
             angular.forEach(ctrl.shuffledItems, function (value, key) {
                 value.done = true;
-                if(value.index === value.order) {
+                if (value.index === value.order) {
                     //ok
                     value.valid = true;
                     result++;
                 }
             });
-            //console.log(ctrl.shuffledItems);
-            if(result === ctrl.shuffledItems.length) {
+            if (result === ctrl.shuffledItems.length) {
                 //gagné
                 $scope.$emit('readevent', true);
             }
@@ -75,8 +91,16 @@
         ctrl.correction = function () {
             //orderBy on order property
             $scope.$emit('readevent', true);
+
+            angular.forEach(ctrl.shuffledItems, function (value, key) {
+                value.valid = true;
+                value.index = value.order;
+            });
+
+            //tri sur index property
+            ctrl.shuffledItems = orderBy(ctrl.shuffledItems, 'index', false);
         };
-    }
+    };
 
     function shuffle(items) {
         for (var i = items.length; i-- > 1;) {
@@ -85,6 +109,6 @@
             items[i] = items[j];
             items[j] = tmp;
         }
-    }
+    };
 
 })();
