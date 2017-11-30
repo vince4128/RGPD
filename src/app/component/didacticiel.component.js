a54dbbe72af98ab41ae8d903282b3b50bed32325
sberthe <sberthe@cylande.com>
2017-11-30 17:42:54 +0100

    Integration of ngDropover

--- src/app/component/didacticiel.component.js
+++ src/app/component/didacticiel.component.js
@@ -20,24 +20,33 @@
 
         ctrl.currentItem = null;
         ctrl.checkReadEvent = checkReadEvent;
+        
+        ctrl.doOptions = {
+            closeWhenClickOff: false,
+            position: 'bottom',
+            triggerEvent: 'none',
+            verticalOffset: 10,
+            staticOptions: false
+        };
+
         /////////////////////////////////////
 
-        function checkReadEvent(){
+        function checkReadEvent() {
             var allSeen = false;
-            for(var i=0; i<ctrl.item.content.pointers.length;i++){
-                if(!ctrl.item.content.pointers[i].seen){
+            for (var i = 0; i < ctrl.item.content.pointers.length; i++) {
+                if (!ctrl.item.content.pointers[i].seen) {
                     return;
-                }else{
+                } else {
                     allSeen = true;
                 }
             }
-            if(allSeen){
-                $scope.$emit('readevent',true);
+            if (allSeen) {
+                $scope.$emit('readevent', true);
             }
         }
 
         ctrl.$onInit = function () {
-            //ici nous avons accès aux valeurs de nos bindings
+            $scope.$emit('ngDropover.open', 'myDropover0');
         };
 
     }
--- src/app/core/core.module.js
+++ src/app/core/core.module.js
@@ -5,7 +5,7 @@
             /*
             *   Angular module
             */
-            'ui.router','ngTouch','ngAnimate','ngSanitize','ui-notification','ngDialog','pascalprecht.translate',
+            'ui.router','ngTouch','ngAnimate','ngSanitize','ui-notification','ngDialog','pascalprecht.translate','ngDropover',
             /*
             *   Scorm Wrapper
             */
