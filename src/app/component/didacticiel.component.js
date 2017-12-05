c99142b11745877561899c57f30c2929de0288e7
sberthe <sberthe@cylande.com>
2017-12-05 14:48:00 +0100

    tutorial activity now displays texts of pointers in popovers, one after the other

--- src/app/component/didacticiel.component.js
+++ src/app/component/didacticiel.component.js
@@ -20,8 +20,15 @@
 
         ctrl.currentItem = 0;
         ctrl.checkReadEvent = checkReadEvent;
-        ctrl.test = test;
         ctrl.close = close;
+        ctrl.currentOpened = false;
+
+        ctrl.doOptions = {
+            closeWhenClickOff: false,
+            position: 'bottom-left',
+            verticalOffset: 10,
+            triggerEvent: 'none'
+          };
 
         /////////////////////////////////////
 
@@ -39,22 +46,38 @@
             }
         }
 
-        function test(){
-            $scope.$emit('ngDropover.open', 'myDropover0');
-        }
-
         function close(index){
             var currentDropover = 'myDropover'+index;
             $rootScope.$emit('ngDropover.close', currentDropover);
-            ctrl.currentItem = parseInt(index)+1;
-            console.log(ctrl.currentItem);
         }
         
         ctrl.$doCheck = function () {
-            $scope.$emit('ngDropover.open', 'myDropover0');
-
-            console.log('prout', angular.element(document.querySelector('#myDropover0')))
+            var currentDropover = 'myDropover'+ctrl.currentItem;
+            if(!ctrl.currentOpened) {
+                if(!angular.element(document.querySelector('#'+currentDropover)).hasClass('ngdo-open')) {
+                    $scope.$emit('ngDropover.open', currentDropover);
+                }
+                else {
+                    ctrl.currentOpened = true;
+                }
+            }
         };
+
+        $rootScope.$on('ngDropover.closing', function(event, dropObj) {
+            ctrl.currentItem++;
+            ctrl.currentOpened = false;
+
+            if(ctrl.currentItem >= ctrl.item.content.pointers.length) {
+                alert("Terminé !");
+                // Do something
+            }
+            //console.log('closing', dropObj.id);
+        });
+
+        $rootScope.$on('ngDropover.opening', function(event, dropObj) {
+            // Do something
+            //console.log('opening', dropObj.id);
+        });
 
     }
 
